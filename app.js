e55158eb1392f2459b29315fb39722c0e6c2916b
Alison Souza <alisonranier@gmail.com>
2023-05-21 19:41:13 -0300

    Removendo middleware hardcoded

--- app.js
+++ app.js
@@ -68,26 +68,26 @@ rabbitMQHandler((connection) => {
 app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-  // // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+//   // // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//   // Request headers you wish to allow
+//   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  if (req.method === 'OPTIONS') res.sendStatus(200);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   if (req.method === 'OPTIONS') res.sendStatus(200);
   
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 app.use('/api', router)
 
 
