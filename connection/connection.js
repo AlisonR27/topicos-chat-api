6c81445b2c977950645081467471de1d3de74eb0
Alison Souza <alisonranier@gmail.com>
2023-05-21 18:37:15 -0300

    Stop using the vHosts

--- connection/connection.js
+++ connection/connection.js
@@ -3,7 +3,7 @@ var amqp = require('amqplib/callback_api')
 
 module.exports = (callback) => {
   const {AMQP_HOST, AMQP_PORT, AMQP_ROUTE } = process.env;
-  amqp.connect(`${AMQP_HOST}:${AMQP_PORT}/${AMQP_ROUTE}`,
+  amqp.connect(`${AMQP_HOST}:${AMQP_PORT}`,
     {
       timeout: 2000
     },
